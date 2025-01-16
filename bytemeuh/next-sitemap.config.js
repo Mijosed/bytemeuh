2a97a219c7ac7ae88da046f71cf5658bac1cc086
Mijosed <mijosed@gmail.com>
2025-01-16 12:43:58 +0100

    save

--- bytemeuh/next-sitemap.config.js
+++ bytemeuh/next-sitemap.config.js
@@ -1,24 +1,22 @@
 module.exports = {
-  siteUrl: 'https://www.bytemeuh-zoo.fr/', // URL de base du site
-  generateRobotsTxt: true, // Génère automatiquement un fichier robots.txt
+  siteUrl: 'https://www.bytemeuh-zoo.fr/', 
+  generateRobotsTxt: true, 
   robotsTxtOptions: {
       policies: [
-          { userAgent: '*', allow: '/' }, // Autorise tous les robots à explorer le site
+          { userAgent: '*', allow: '/' }, 
       ],
       additionalSitemaps: [
-          'https://www.bytemeuh-zoo.fr/sitemap-0.xml', // Ajoutez des sitemaps personnalisés si nécessaire
+          'https://www.bytemeuh-zoo.fr/sitemap-0.xml', 
       ],
   },
-  changefreq: 'daily', // Fréquence de changement estimée des pages
-  priority: 0.7, // Priorité par défaut pour les pages
-  sitemapSize: 5000, // Taille maximale des fichiers sitemap (par défaut : 5000 URLs par fichier)
+  changefreq: 'daily', 
+  priority: 0.7, 
   transform: async (config, path) => {
-      // Transformer chaque URL avant de l'ajouter au sitemap
       return {
-          loc: path, // URL
-          changefreq: config.changefreq, // Fréquence de changement
-          priority: path === '/' ? 1.0 : 0.7, // Donne une priorité élevée à la page d'accueil
-          lastmod: new Date().toISOString(), // Date de dernière modification
+          loc: path, 
+          changefreq: config.changefreq, 
+          priority: path === '/' ? 1.0 : 0.7, 
+          lastmod: new Date().toISOString(), 
       };
   },
 };
